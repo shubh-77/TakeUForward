c5f6caf092843d804afcdbd2b3fb858b3a313da9
shubh-77 <shubhamphansekar90@gmail.com>
2026-06-11 22:59:57 +0530

    exception handling

--- JavaBasics/src/com/oops/basics/exceptionhandling/ExceptionHandling.java
+++ JavaBasics/src/com/oops/basics/exceptionhandling/ExceptionHandling.java
@@ -4,11 +4,14 @@ public class ExceptionHandling {
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 0;
-        int num3;
+        int num3 = 0;
         try {
             num3 = num1 / num2;
+            System.out.println(num3);
         } catch (Exception e) {
             System.out.println(e.toString());
+        } finally {
+            System.out.println("finally block execute whether exception occurs or not");
         }
     }
 }
